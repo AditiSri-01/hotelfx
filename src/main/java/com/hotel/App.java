889edc7b231f0d862f5eb4d2f770664511b7a1fc
AditiSri-01 <aditiishri@gmail.com>
2026-04-06 01:17:59 +0530

    Update App.java

--- src/main/java/com/hotel/App.java
+++ src/main/java/com/hotel/App.java
@@ -1,1 +1,27 @@
+package com.hotel;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/hotel/view/main.fxml"));
+        Scene scene = new Scene(loader.load(), 1100, 750);
+
+        // Load the CSS to apply the dark theme
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        stage.setTitle("Premium Hotel Management System");
+        stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(700);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
