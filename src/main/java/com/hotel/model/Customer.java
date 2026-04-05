16eb85e0e8c5a54dc4fbd0d06ffb4a86cc26f051
AditiSri-01 <aditiishri@gmail.com>
2026-04-06 01:19:21 +0530

    Create Customer.java

---
+++ src/main/java/com/hotel/model/Customer.java
@@ -0,0 +1,21 @@
+package com.hotel.model;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
